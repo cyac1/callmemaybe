886c20b221f5c6cf0b5f589d8ae83ad102f5b309
Jan Henning <jh.jestaedt@gmx.de>
2018-07-12 20:56:23 +0200

    Revert "profile and dashboard"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-  before_action :authenticate_user!, only: [:dashboard]
 
   def home
-  end
-
-  def dashboard
-
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -68,15 +68,13 @@ User.create!(user_attributes)
 puts 'Creating users...'
 
 10.times do
-  full_name_array = Faker::RickAndMorty.character.split
   user_attributes = [
-
     { email: Faker::Internet.email,
       password: "password",
       password_confirmation: "password",
       phone_number: Faker::PhoneNumber.phone_number,
-      first_name: full_name_array[0],
-      last_name: full_name_array[1],
+      first_name: "Ben",
+      last_name: "Stuart",
       skype_username: Faker::Internet.user_name,
       whatsapp_number: Faker::PhoneNumber.phone_number,
       street: Faker::Address.street_address,
@@ -91,7 +89,7 @@ puts 'Creating users...'
 end
 
 # ## Import question data ##
-q_api_response = JSON.parse(open("https://opentdb.com/api.php?amount=55&type=multiple").read)
+q_api_response = JSON.parse(open("https://opentdb.com/api.php?amount=50&type=multiple").read)
 #binding.pry
 
 
@@ -108,6 +106,7 @@ users = User.all
   counter = 0
 users.each do |user|
   5.times do
+    break if counter == 50
 
     difficulty = q_api_response["results"][counter]["difficulty"]
     category_name = q_api_response["results"][counter]["category"]
@@ -124,8 +123,8 @@ users.each do |user|
     question = q_api_response["results"][counter]["question"]
     counter += 1
 
-    question_attributes =  { title: question,
-        description: Faker::TheITCrowd.quote,
+    question_attributes =  { title: "#{difficulty.capitalize} #{category.name} question",
+        description: question,
         user_id: user.id,
         category_id: category.id }
 
