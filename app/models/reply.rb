3c6924ebd66ceea30c648be3ae36234105f43237
stephanie cattoir <stephanie.cattoir@gmail.com>
2018-07-16 16:38:10 +0200

    changing status of reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,4 +4,24 @@ class Reply < ApplicationRecord
   belongs_to :question
   has_one :call
   enum reply_status: [:pending, :accepted, :rejected]
+
+  def accepted?
+    @reply_status == :accepted
+  end
+
+  def pending?
+    @reply_status == :pending
+  end
+
+  def rejected?
+    @reply_status == :rejected
+  end
+
+  def accept_reply
+    @reply_status = 1
+  end
+
+  def reject_reply
+    @reply_status = 2
+  end
 end
