803b660c735722bc4bf9e5a43356af107bd5fab8
LuigiSantoro <lg_santoro@virgilio.it>
2018-07-17 19:07:11 +0200

    footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,22 @@
 //= require rails-ujs
 //= require_tree .
+
+
+
+
+const footer = document.querySelector('.footer');
+
+  if (footer) {
+    window.addEventListener('scroll', () => {
+      console.log("scrollY " + window.scrollY);
+      console.log("inner " + window.innerHeight);
+      console.log(document.body.scrollHeight);
+      const height = (document.body.scrollHeight - 100);
+      console.log(height);
+      if (window.scrollY + window.innerHeight >= height ) {
+        footer.style.visibility = "visible";
+      } else {
+        footer.style.visibility = "hidden";
+      }
+    });
+  }
