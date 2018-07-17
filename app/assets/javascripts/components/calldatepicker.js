77244f442be723fb8603cf441ee1f67f50f0893c
stephanie cattoir <stephanie.cattoir@gmail.com>
2018-07-17 18:53:32 +0200

    giving lead to Jan (for once)

--- app/assets/javascripts/components/calldatepicker.js
+++ app/assets/javascripts/components/calldatepicker.js
@@ -1,23 +1,35 @@
 let datePic = document.querySelector(".datepicker");
 let calendar = document.querySelector(".time-picker");
 calendar.hidden = true;
+let timeslots = document.querySelectorAll(".timeslot");
+let setCall = document.getElementById("setCall");
+
+let url = new URL(window.location.href );
 
 datePic.addEventListener('change', function() {
-  console.log(datePic.getAttribute("value"))
   if (datePic.getAttribute("value") ) {
     calendar.hidden = false;
   }
 })
 
-let timeslots = document.querySelectorAll(".timeslot");
-console.log(timeslots)
-
 timeslots.forEach(function(timeslot) {
   timeslot.addEventListener('click', function() {
     timeslots.forEach(function(time){
-      timeslot.classList.remove("selected");
+      time.classList.remove("time-selected");
     })
     console.log(event)
-    timeslot.classList.add("selected");
+    timeslot.classList.add("time-selected");
   });
 });
+
+setCall.addEventListener('click', function() {
+  let dateSelected = datePic.getAttribute("value");
+  let timeSelected = document.querySelector(".time-selected").innerText;
+  // console.log(dateSelected)
+  // console.log(timeSelected)
+  let base = new URL(window.location.href );
+
+  base = base + "?day=" + encodeURIComponent(dateSelected) + "&time=" + encodeURIComponent(timeSelected);
+  console.log(base)
+  window.location = base;
+})
